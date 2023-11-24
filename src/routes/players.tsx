e613af3cdb62efdd66033b01ac82e3ffe1807b47
coDribble <sksrlatkddn@naver.com>
2023-11-24 16:56:46 +0900

    231124 선수 검색 완성?

--- src/routes/players.tsx
+++ src/routes/players.tsx
@@ -30,12 +30,9 @@ const List = styled.div`
 
 export default function Players() {
   const [isLoading, setIsLoading] = useState(true);
-  const [isSearching, setIsSearching] = useState(false);
+  const [isSearch, setIsSearch] = useState(false);
   const [isAutoSearch, setIsAutoSearch] = useState(false);
-  const [searchType, setSearchType] = useState<
-    "all" | "keywords" | "autoKeywords"
-  >("all");
-  const [isShow, setIsShow] = useState(false);
+  const [isShowRelated, setIsShowRelated] = useState(false);
   const [players, setPlayers] = useState<IPlayerInfo[]>([]);
   const [searchPlayers, setSearchPlayers] = useState<IPlayerInfo[]>([]);
   const [keywords, setKeywords] = useState("");
@@ -45,7 +42,7 @@ export default function Players() {
   const [page, setPage] = useState(1);
   const limit = 50;
   const offset = (page - 1) * limit;
-  const scrollRef = useRef<HTMLDivElement>(null);
+  const inputRef = useRef<HTMLInputElement>(null);
   const listRef = useRef<HTMLUListElement>(null);
   const focusRef = useRef<HTMLAnchorElement>(null);
 
@@ -63,23 +60,13 @@ export default function Players() {
 
   const onKeywordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { value } = e.target;
+
     const regEx = /^[a-zA-Z0-9가-힣]+$/;
     const searchKeyword = value
       .toString()
       .toLocaleLowerCase()
       .split(" ")
       .join("");
-
-    if (isAutoSearch) {
-      const enteredValue =
-        e.nativeEvent.inputType === "deleteContentBackward"
-          ? ""
-          : e.nativeEvent.data;
-      focusIndex >= 0 && setKeywords(autoKeywords + enteredValue);
-      setIsAutoSearch(false);
-      setFocusIndex(-1);
-      return;
-    }
 
     if (value.length > 1) {
       const uniqueName = [...new Set(players.map((data) => data.name))]
@@ -99,12 +86,20 @@ export default function Players() {
       setRelatedKeywords(uniqueName);
     }
 
-    setIsShow(value.length > 1);
-    setKeywords(value);
-  };
+    if (isAutoSearch) {
+      const enteredValue =
+        (e.nativeEvent as InputEvent).inputType === "deleteContentBackward"
+          ? ""
+          : (e.nativeEvent as InputEvent).data;
 
-  const onFocus = () => {
-    setIsShow(true);
+      focusIndex >= 0 && setKeywords(autoKeywords + enteredValue);
+      setIsAutoSearch(false);
+      setFocusIndex(-1);
+      return;
+    }
+
+    setIsShowRelated(value.length > 0);
+    setKeywords(value);
   };
 
   const onKeyUp = (e: React.KeyboardEvent<HTMLInputElement>) => {
@@ -113,14 +108,14 @@ export default function Players() {
     if (e.key === "ArrowDown") {
       if (relatedKeywords.length === 0) return;
 
+      if (!isShowRelated) setIsShowRelated(true);
+
       if (listRef.current?.childElementCount === focusIndex + 1) {
         setFocusIndex(() => 0);
         return;
       }
 
-      if (focusIndex === -1) {
-        setIsAutoSearch(true);
-      }
+      if (focusIndex === -1) setIsAutoSearch(true);
 
       setFocusIndex((index) => index + 1);
       setAutoKeywords(relatedKeywords[focusIndex + 1]);
@@ -130,6 +125,7 @@ export default function Players() {
       if (focusIndex === -1) return;
 
       if (focusIndex === 0) {
+        setIsShowRelated(false);
         setAutoKeywords("");
         setFocusIndex((index) => index - 1);
         setIsAutoSearch(false);
@@ -154,60 +150,32 @@ export default function Players() {
 
     const name = e.currentTarget.innerText;
 
-    setAutoKeywords(name);
     setKeywords(name);
-    setIsAutoSearch(true);
+    setFocusIndex(-1);
+    setIsAutoSearch(false);
+    inputRef.current?.focus();
   };
 
   const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    let searchResult: IPlayerInfo[] = [];
-
-    if (searchType === "all") {
-      searchResult = players;
-    } else if (searchType === "keywords") {
-      searchResult = players.filter((data: IPlayerInfo) =>
-        data.name
-          .toLowerCase()
-          .split(" ")
-          .join()
-          .includes(keywords.toLowerCase().split(" ").join())
-      );
-    } else if (searchType === "autoKeywords") {
-      searchResult = players.filter((data: IPlayerInfo) =>
-        data.name
-          .toLowerCase()
-          .split(" ")
-          .join()
-          .includes(autoKeywords.toLowerCase().split(" ").join())
-      );
-    }
-
-    setPage(1);
-    setSearchPlayers([...new Set(searchResult)]);
-    setIsSearching(true);
-    setIsShow(false);
-  };
-
-  useEffect(() => {
-    setSearchType(
-      keywords ? "keywords" : autoKeywords ? "autoKeywords" : "all"
+    const filteredPlayers = players.filter((player) =>
+      player.name
+        .toLowerCase()
+        .split(" ")
+        .join("")
+        .includes(
+          (isAutoSearch ? autoKeywords : keywords)
+            .toLowerCase()
+            .split(" ")
+            .join("")
+        )
     );
-  }, [keywords, autoKeywords]);
 
-  useEffect(() => {
-    setTimeout(() => {
-      listRef.current?.scrollIntoView({
-        behavior: "smooth",
-        block: "center",
-      });
-    }, 100);
-  }, [focusIndex]);
-
-  console.log(
-    `searchType: ${searchType}, autoKeywords: ${autoKeywords}, keywords: ${keywords}`
-  );
+    setIsSearch(keywords.length > 0);
+    setSearchPlayers(filteredPlayers);
+    setIsShowRelated(false);
+  };
 
   return (
     <>
@@ -229,8 +197,8 @@ export default function Players() {
             <input
               id="name"
               type="text"
+              ref={inputRef}
               value={isAutoSearch ? autoKeywords : keywords}
-              onFocus={onFocus}
               onKeyUp={onKeyUp}
               onChange={onKeywordChange}
               className="w-[200px] px-2 py-2 text-black"
@@ -242,48 +210,43 @@ export default function Players() {
               className="w-[60px] bg-indigo-600 cursor-pointer"
             />
           </div>
-          {isShow && (
-            <div
-              ref={scrollRef}
-              className="overflow-hidden overflow-y-auto absolute top-full w-[200px] max-h-[300px] bg-gray-50 border border-solid border-black z-[1]"
-            >
-              {relatedKeywords?.length > 0 && (
-                <ul
-                  role="list"
-                  ref={listRef}
-                >
-                  {relatedKeywords.map((data, index) => (
-                    <li key={index}>
-                      <a
-                        href="#"
-                        onClick={onInsertKeyword}
-                        className={`block w-full p-[10px] ${
-                          index === focusIndex
-                            ? "bg-indigo-400 text-white"
-                            : "bg-white text-gray-900"
-                        } hover:bg-indigo-400 hover:text-white`}
-                        ref={index === focusIndex ? focusRef : undefined}
-                      >
-                        {data
-                          .split(new RegExp(`(${keywords})`, "ig"))
-                          .map((part) =>
-                            part.toLowerCase() ===
-                            keywords.toLocaleLowerCase() ? (
-                              <span
-                                key={part}
-                                className="font-bold text-indigo-600"
-                              >
-                                {part}
-                              </span>
-                            ) : (
-                              part
-                            )
-                          )}
-                      </a>
-                    </li>
-                  ))}
-                </ul>
-              )}
+          {isShowRelated && relatedKeywords?.length > 0 && (
+            <div className="overflow-hidden overflow-y-auto absolute top-full w-[200px] max-h-[300px] bg-gray-50 border border-solid border-black z-[1]">
+              <ul
+                role="list"
+                ref={listRef}
+              >
+                {relatedKeywords.map((data, index) => (
+                  <li key={index}>
+                    <a
+                      href="#"
+                      ref={focusRef}
+                      onClick={onInsertKeyword}
+                      className={`block w-full p-[10px] ${
+                        focusIndex === index
+                          ? "bg-indigo-400 text-white"
+                          : "bg-white text-gray-900"
+                      } hover:bg-indigo-400 hover:text-white`}
+                    >
+                      {data
+                        .split(new RegExp(`(${keywords})`, "ig"))
+                        .map((part) =>
+                          part.toLowerCase() ===
+                          keywords.toLocaleLowerCase() ? (
+                            <span
+                              key={part}
+                              className="font-bold text-indigo-600"
+                            >
+                              {part}
+                            </span>
+                          ) : (
+                            part
+                          )
+                        )}
+                    </a>
+                  </li>
+                ))}
+              </ul>
             </div>
           )}
         </div>
@@ -294,23 +257,21 @@ export default function Players() {
           role="list"
           className="flex flex-col flex-wrap gap-3"
         >
-          {isSearching ? (
-            searchPlayers.length > 0 ? (
-              searchPlayers
-                .slice(offset, offset + limit)
-                .map((player: IPlayerInfo) => (
-                  <Player
-                    key={player.id}
-                    {...player}
-                  />
-                ))
+          {isSearch ? (
+            searchPlayers.length ? (
+              searchPlayers.slice(offset, offset + limit).map((player) => (
+                <Player
+                  key={player.id}
+                  {...player}
+                />
+              ))
             ) : (
               <li className="w-full py-10 text-center">
-                검색된 선수가 없습니다.
+                {`'${keywords}'로 검색된 선수가 없습니다.`}
               </li>
             )
-          ) : players.length > 0 ? (
-            players.slice(offset, offset + limit).map((player: IPlayerInfo) => (
+          ) : players.length ? (
+            players.slice(offset, offset + limit).map((player) => (
               <Player
                 key={player.id}
                 {...player}
@@ -318,15 +279,13 @@ export default function Players() {
             ))
           ) : (
             <li className="w-full py-10 text-center">
-              {isLoading
-                ? "선수 목록 구성중..."
-                : `${keywords ? "검색된" : "등록된"} 선수가 없습니다.`}
+              {isLoading ? "선수 목록 생성중..." : "등록된 선수가 없습니다."}
             </li>
           )}
         </ul>
 
         <Pagination
-          total={isSearching ? searchPlayers.length : players.length}
+          total={searchPlayers.length}
           limit={limit}
           page={page}
           setPage={setPage}
