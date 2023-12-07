e16397ecc4585a95e684c510f447ec40b65fdf6d
coDribble <sksrlatkddn@naver.com>
2023-12-07 18:20:35 +0900

    231207 매치기록 리스트 수정
    승패기록 가져옴

--- src/components/match.tsx
+++ src/components/match.tsx
@@ -8,7 +8,7 @@ interface IMatchId {
   userId: string;
 }
 
-export default function MatchItem({ matchId, userId }: IMatchId) {
+export default function MatchItem({ matchId }: IMatchId) {
   const [matchData, setMatchData] = useState<IMatchData>();
 
   useEffect(() => {
@@ -29,6 +29,7 @@ export default function MatchItem({ matchId, userId }: IMatchId) {
   }, [matchId]);
 
   // console.log(matchDetail);
+  // console.log(matchData?.matchInfo);
 
   return (
     <li className="w-[calc((100%-50px)/2)]">
@@ -37,11 +38,54 @@ export default function MatchItem({ matchId, userId }: IMatchId) {
         className="flex flex-col gap-2"
       >
         <div className="flex justify-between">
-          {matchData?.matchType &&
-            matchData?.matchType < 200 &&
-            matchData?.matchInfo
-              .filter((data) => data.accessId !== userId)
-              .map((data, i) => <p key={i}>vs {data.nickname}</p>)}
+          {matchData?.matchType && matchData?.matchType < 200 && (
+            <div className="flex items-center justify-between w-[calc(100%-200px)]">
+              {matchData?.matchInfo
+                // .filter((data) => data.accessId !== userId)
+                .map((data, i) =>
+                  i % 2 === 0 ? (
+                    <>
+                      <p
+                        key={i}
+                        className="flex gap-[10px]"
+                      >
+                        <span
+                          className={`${
+                            data.matchDetail.matchResult === "승"
+                              ? "text-indigo-600"
+                              : data.matchDetail.matchResult === "무"
+                              ? "text-yellow-600"
+                              : "text-red-600"
+                          } font-bold`}
+                        >
+                          {data.matchDetail.matchResult}
+                        </span>
+                        <span>{data.nickname}</span>
+                      </p>
+                      <span>vs</span>
+                    </>
+                  ) : (
+                    <p
+                      key={i}
+                      className="flex gap-[10px]"
+                    >
+                      <span>{data.nickname}</span>
+                      <span
+                        className={`${
+                          data.matchDetail.matchResult === "승"
+                            ? "text-indigo-600"
+                            : data.matchDetail.matchResult === "무"
+                            ? "text-yellow-600"
+                            : "text-red-600"
+                        } font-bold`}
+                      >
+                        {data.matchDetail.matchResult}
+                      </span>
+                    </p>
+                  )
+                )}
+            </div>
+          )}
           <p>{moment(matchData?.matchDate).format("YYYY-MM-DD HH:mm:ss")}</p>
         </div>
       </Link>
