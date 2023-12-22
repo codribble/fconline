b593a50848206e7ac26c7f7033212a1c8f9e1b26
coDribble <sksrlatkddn@naver.com>
2023-12-22 17:54:30 +0900

    231222 매치기록이 있는 매치타입만 가져오기

--- src/components/users/user_match.tsx
+++ src/components/users/user_match.tsx
@@ -15,23 +15,75 @@ export interface IMatchType {
 export default function UserMatchList({ ouid }: IUserInfo) {
   const [isLoadedMatch, setIsLoadedMatch] = useState(false);
   const [matchType, setMatchType] = useState<IMatchType[]>([]); // 모든 매치 데이터
-  const [selectedType, setSelectedType] = useState<number>();
-  const [matchRecord, setMatchRecord] = useState([]);
+  const [selectedType, setSelectedType] = useState<number | null>(null);
+  const [matchRecord, setMatchRecord] = useState<string[]>([]);
   // const swiperRef = useRef<SwiperRef>(null);
+  // const storageType = sessionStorage.getItem("SelectedMatchType");
 
   useEffect(() => {
     const storageType = sessionStorage.getItem("SelectedMatchType");
 
-    fetch("https://open.api.nexon.com/static/fconline/meta/matchtype.json")
-      .then((res) => res.json())
-      .then((data) => {
-        data.map((match: IMatchType, i: number) => {
-          storageType
-            ? setSelectedType(JSON.parse(storageType))
-            : i === 0 && setSelectedType(Number(match.matchtype));
-        });
-        setMatchType(data);
-      });
+    const fetchMatchTypes = async () => {
+      try {
+        const headers = {
+          "x-nxopen-api-key": import.meta.env.VITE_FCONLINE_API_KEY,
+        };
+
+        const matchTypeResponse = await fetch(
+          "https://open.api.nexon.com/static/fconline/meta/matchtype.json"
+        );
+
+        if (matchTypeResponse.ok) {
+          const matchTypeData = await matchTypeResponse.json();
+
+          const updatedMatchType: IMatchType[] = await Promise.all(
+            matchTypeData.map(async (type: IMatchType) => {
+              const matchDataResponse = await fetch(
+                `https://open.api.nexon.com/fconline/v1/user/match?ouid=${ouid}&matchtype=${type.matchtype}`,
+                { headers }
+              );
+
+              if (matchDataResponse.ok) {
+                const matchList = await matchDataResponse.json();
+                if (matchList.length > 0) {
+                  return {
+                    matchtype: type.matchtype,
+                    desc: type.desc,
+                  };
+                }
+              } else {
+                console.error(
+                  `Failed to fetch match data for matchtype ${type.matchtype}`
+                );
+              }
+
+              return null; // If no match data, return null
+            })
+          );
+
+          const filteredMatchType = updatedMatchType.filter(
+            Boolean
+          ) as IMatchType[];
+
+          if (filteredMatchType.length > 0) {
+            storageType
+              ? setSelectedType(JSON.parse(storageType))
+              : setSelectedType(filteredMatchType[0].matchtype);
+          }
+
+          setMatchType(filteredMatchType);
+        } else {
+          console.error(
+            "Failed to fetch match types",
+            matchTypeResponse.status
+          );
+        }
+      } catch (error) {
+        console.error("Error fetching match data:", error);
+      }
+    };
+
+    fetchMatchTypes();
   }, [ouid]);
 
   useEffect(() => {
@@ -47,7 +99,7 @@ export default function UserMatchList({ ouid }: IUserInfo) {
         .then((res) => res.json())
         .then((data) => {
           setMatchRecord(data);
-          sessionStorage.setItem("MatchRecord", JSON.stringify(data));
+          // sessionStorage.setItem("MatchRecord", JSON.stringify(data));
         })
         .catch((error) =>
           console.error("매치 데이터를 불러오는 중 오류가 발생했습니다.", error)
@@ -63,7 +115,7 @@ export default function UserMatchList({ ouid }: IUserInfo) {
       <div
         id="matchCategory"
         role="tablist"
-        className="flex items-center gap-2 mb-5"
+        className="flex flex-wrap items-center gap-2 mb-5"
       >
         {matchType &&
           matchType.map((match) => (
@@ -88,7 +140,7 @@ export default function UserMatchList({ ouid }: IUserInfo) {
       </div>
 
       {matchRecord.length > 0 && isLoadedMatch ? (
-        <ul className="flex flex-wrap gap-[15px]">
+        <ul className="flex flex-wrap gap-y-[15px] gap-x-[50px]">
           {matchRecord.map((id, i) => (
             <MatchItem
               key={i}
@@ -99,7 +151,7 @@ export default function UserMatchList({ ouid }: IUserInfo) {
         </ul>
       ) : (
         <div className="py-[30px] text-center">
-          <p>매치 기록이 없습니다.</p>
+          <p>매치 기록을 조회중...</p>
         </div>
       )}
     </>
