9a37d8da49a76bc35acad49bbec3895431521aec
coDribble <sksrlatkddn@naver.com>
2023-12-22 17:54:47 +0900

    231222 디자인 수정

--- src/components/tier.tsx
+++ src/components/tier.tsx
@@ -3,10 +3,8 @@ import moment from "moment";
 
 export default function Tier({ desc, tier, achievementDate }: IBestTier) {
   return (
-    <li className="flex flex-col gap-[10px] relative w-[calc(50%-10px)] p-5 border border-white border-solid rounded-2xl transition-all hover:shadow-md hover:shadow-white">
-      <p className="font-semibold text-xl">
-        {desc?.includes("모드") ? desc : desc + " 모드"}
-      </p>
+    <li className="flex flex-col gap-[10px] relative w-full p-5 border border-white border-solid rounded-2xl transition-all hover:shadow-md hover:shadow-white md:w-[calc(50%-10px)]">
+      <p className="font-semibold text-xl">{desc}</p>
       <div className="flex justify-between">
         <p>{tier}</p>
         <time>
